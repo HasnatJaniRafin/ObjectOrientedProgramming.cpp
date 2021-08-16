ee5f47151c8b43cfebaf3c5b9278d7861b6e9e66
HASNAT JANI RAFIN <77789741+HasnatJaniRafin@users.noreply.github.com>
2021-08-17 00:07:38 +0800

    Update friendClass.cpp

--- friendClass.cpp
+++ friendClass.cpp
@@ -1,1 +1,33 @@
 
+
+#include <iostream>
+
+using namespace std;
+
+class A 
+{
+    private :
+           int id = 2507 ;
+           string name = " HASNAT JANI RAFIN";
+    public :
+        friend class B ;
+};
+class B 
+{
+    public :
+         void display (A obj)
+         {
+             cout <<"Class A - student ID : " <<obj.id <<endl ;
+             cout << "         student name : " <<obj.name <<endl;
+         }
+    
+};
+
+int main()
+{
+    A obj1;
+    B obj2 ;
+    
+    obj2.display(obj1);
+    
+}
